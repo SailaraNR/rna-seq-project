a978ff1a1164cf47e16c7ba767a490222b82adeb
lau-bgg <laurabarrealesgomez@MacBook-Air-de-Laura.local>
2025-05-09 19:32:58 +0200

    Solo he comentado. Falta probarlo

--- 01-pre_fastqc/pre_fastqc.sh
+++ 01-pre_fastqc/pre_fastqc.sh
@@ -3,10 +3,12 @@
 #Los archivos que acepta solo son archivos .fastq 
 #Author: Laura Barreales and Sara Lévano
 #Start date: 4th May 2025
-#Version: 1.2
+version= Version 1.5
 
 #Usage example: pre-fastqc.sh
 #No arguments expected, but -v and -h are available
+#link al manual de FastQC: https://github.com/s-andrews/FastQC/blob/master/fastqc
+#link al manual de MultiQC. No es de github, pero he accedido desde github:https://docs.seqera.io/multiqc/getting_started/running_multiqc 
 
 #######################################################################
 
@@ -31,7 +33,7 @@ while getopts "hv" opt; do
             "This script checks the quality of the previously downloaded raw sequences" | tee -a logs/stdout
             echo -e "Usage example:\n\t sh $0 -h: Provides help \n\t sh $0 -v: Tells script's version\n\tsh $0: checks quality score of raw sequences" | tee -a logs/stdout
             exit 0;;
-        v) echo "Version 1.2" | tee -a logs/stdout
+        v) echo "$version" | tee -a logs/stdout
             exit 0;;
         \?) echo -e "Invalid option\n\ 
             Usage example: $0 " | tee -a logs/stderr
@@ -39,8 +41,8 @@ while getopts "hv" opt; do
     esac
 done
 
-input_path="/home/saral_ubuntu/bioprogra/rna-seq-project/00-raw_data"
-output_path="/home/saral_ubuntu/bioprogra/rna-seq-project/01-pre_fastqc"
+input_path="/home/saral_ubuntu/bioprogra/rna-seq-project/00-raw_data/results"
+output_path="/home/saral_ubuntu/bioprogra/rna-seq-project/01-pre_fastqc/results"
 
 # Check if the target is not a directory
 if [ ! -d "$input_path" ]; then
@@ -50,7 +52,8 @@ fi
 
 for file in "$input_path"; do
         #Vamos a comprobar que el archivo no está vacío
-        echo "Checking if $file exists and it's not empty" | tee -a logs/stdout
+        sample=$(basename "$file") #Nos quedamos con el nombre de la muestra
+        echo "Checking if $sample exists and it's not empty" | tee -a logs/stdout
         if [[ -f "$file" && -s "$file" ]]; then
             echo "$file exist and is not empty"  | tee -a logs/stdout
          else
@@ -60,10 +63,10 @@ for file in "$input_path"; do
 
         #Vamos a comprobar que el arhivo es .fastqc
         echo "Checking whether file extension is .fastq" | tee -a logs/stdout
-        if [ "$file" == *.fastq ]; then
+        if [[ "$file" == *.fastq ]]; then
             echo "File extension is correct" | tee -a logs/stdout
             #Vamos a comprobar que el archivo es legible y ejecutable
-            echo "Checking $file permissions" | tee -a logs/stdout
+            echo "Checking $sample permissions" | tee -a logs/stdout
 
             if [[ -r "$file" && -x "$file" ]]; then
                 echo "File is readable and executable" | tee -a logs/stdout
@@ -74,7 +77,7 @@ for file in "$input_path"; do
             elif [ -x "$file" ]; then
                 echo "File is executable but not readable" | tee -a logs/stdout
                 chmod +r "$file"
-                echo "Fiexd" | tee -a logs/stdout
+                echo "Fixed" | tee -a logs/stdout
             else
                 echo "File is neither readable nor executable. Fixing" | tee -a logs/stdout
                 chmod +rx "$file"
@@ -82,28 +85,34 @@ for file in "$input_path"; do
             fi
         
             #Ahora vamos a comprobar que la calidad de la secuencia con fastqc
-            echo "Checking quality score in $file" | tee -a logs/stdout
+            echo "Checking quality score in $sample" | tee -a logs/stdout
+            #creo que podría ponerse así fastqc $file -o $output_path | tee -a logs/stdout, porque $file debería ser la ruta absoluta del archivo (al menos así era en el archivo symlinks)
             fastqc $input_path/*.fastq -o $output_path | tee -a logs/stdout #-o guarda los archivos en esa ruta, que en este caso es la subcarpeta results
-            echo "Fastqc analysis completed for $file" | tee -a logs/stdout
+            echo "Fastqc analysis completed for $sample" | tee -a logs/stdout
         else
-            echo "this is not .fastq, moving to the next..."
+            echo "this is not .fastq, moving to the next..." #Si funciona lo de guardar los symlinks en 00/results no hace falta esto, pero se podría dejar como cortafuegos
         fi; 2>> logs/stderr
 done; 2>> logs/stderr
 
 echo "Now Multiqc will run in order to make a summary of the analysis"
 echo "But first it will check if files' extensions are .html after fastqc analysis"
 #Vamos a comprobar que el arhivo es .html
+for $file in $output_path; do
     echo "Checking whether file extension is .html" | tee -a logs/stdout
-    if [ "$file" == *.html ]; then
+    if [[ "$file" == *.html ]]; then
             echo "File extension is correct" | tee -a logs/stdout
     else
             echo "File extension not correct. Must be .html file" | tee -a logs/stderr
             exit 1
     fi 
-multiqc ./results -n multiqc_analysis.html #multiqc se va a ejecutar en el 01/results que es donde se han guardado las secuencias fastqc
+done 
+#multiqc ./results -n multiqc_analysis.html #multiqc se va a ejecutar en el 01/results que es donde se han guardado las secuencias fastqc
+
+#A lo mejor así queda más elegante
+multiqc $output_path -n multiqc_analysis.html #multiqc se va a ejecutar en el 01/results que es donde se han guardado las secuencias fastqc
 # se habrá creado un archivo html con un resumen de el análisis de fastqc
 # -n es para ponerle nombre al archivo
-echo -e "Multiqc has finished its job. \n You can now check for an .html file in $pwd \n to find the analysis"
+echo -e "Multiqc has finished its job. \nYou can now check for an .html file in $(pwd) \nto find the analysis"
 
 ######################################################
 #Voy a dejar este enlace aquí, ya lo quitaremos.
