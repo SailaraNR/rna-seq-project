3f07ffde80e69233b01c15601895afb2dd2f532b
lau-bgg <laurabarrealesgomez@MacBook-Air-de-Laura.local>
2025-05-12 23:20:04 +0200

    redirecciones hechas

--- 00-raw_data/symlinks.sh
+++ 00-raw_data/symlinks.sh
@@ -7,7 +7,7 @@
 
 #Usage example
 #symlinks.sh -d ruta/absoluta/del/directorio
-version="version 1.2"
+version="version 2.0"
 ############################################################################################
 
 # inicialización de logs vacíos
@@ -15,11 +15,11 @@ cat /dev/null > logs/stdout
 cat /dev/null > logs/stderr
 
 # explain the code
-echo -e "Use: \n\t-h for help\n\t-v for the version of the script \n\t-d ruta/absoluta/del/directorio to create de symbolic links" | tee -a logs/stdout
+{echo -e "Use: \n\t-h for help\n\t-v for the version of the script \n\t-d ruta/absoluta/del/directorio to create de symbolic links" | tee -a logs/stdout
 
 #Vamos a ver si se han proporcionado argumentos
 if [ $# -eq 0 ]; then
-        echo -e "No options provided. \nUse $0 -h for help"| tee -a logs/stdout
+        echo -e "No options provided. \nUse $0 -h for help"
         exit 1
 fi
 
@@ -27,76 +27,80 @@ fi
 while getopts ":d:hv" opt; do
         case $opt in
                 h) echo -e "You asked for usage help\n"\
-                   "This script creates on symbolic link per file.fastq in the directory provided" | tee -a logs/stdout
-                   echo -e "Usage: \n\t-h for help\n\t-v for the version of the script \n\t-d ruta/absoluta/del/directorio to create de symbolic links" | tee -a logs/stdout
+                   "This script creates on symbolic link per file.fastq in the directory provided" 
+                   echo -e "Usage: \n\t-h for help\n\t-v for the version of the script \n\t-d ruta/absoluta/del/directorio to create de symbolic links" 
                    exit 0;;
-                v) echo $version | tee -a logs/stdout
+                v) echo $version 
                    exit 0;;
                 d) directory="$OPTARG"
-                   echo "Procesando el directorio: $directory ..." | tee -a logs/stdout 
+                   echo "Procesando el directorio: $directory ..." 
                     ;;
-                :) echo "Option -$OPTARG requieres an argument" | tee -a logs/stdout #si pone solo -d no sirve
+                :) echo "Option -$OPTARG requieres an argument" >&2 #si pone solo -d no sirve
                    exit 1 ;;
                 \?) echo -e "Invalid option or missing argument\n"\
-                   "Usage example: $0 SRR_Acc_List.txt" | tee -a logs/stdout
+                   "Usage example: $0 SRR_Acc_List.txt" >&2
                    exit 1 ;;
 
         esac
 done
+} >> >(tee -a logs/all_samples.out) 2>> >(tee -a logs/all_samples.err)
 
-for file in "$directory"/*; do
+{for file in "$directory"/*; do
     echo -e "\n"
     sample=$(basename "$file") #Nos quedamos con el nombre de la muestra
+    name=${sample%.*} #quita la extensión del archivo, se usará para dar nombre a los logs de salida
     echo "This is $sample" 
     #Vamos a comprobar que el archivo no está vacío
-    echo "Checking if the file exists and it's not empty" | tee -a logs/stdout
+    echo "Checking if the file exists and it's not empty" 
     if [ -s "$file" ]; then
-            echo "File exist and is not empty"  | tee -a logs/stdout
+            echo "File exist and is not empty"  
     else
-            echo "file is empty"  | tee -a logs/stderr
+            echo "file is empty" >&2
             exit 1
     fi 
 
     #Vamos a comprobar que el arhivo es .fastq
-    echo "Checking whether file extension is .fastq" | tee -a logs/stdout
+    echo "Checking whether file extension is .fastq" 
     if [[ "$file" == *.fastq.gz ]]; then
-            echo "File extension is correct" | tee -a logs/stdout
+            echo "File extension is correct" 
     else
-            echo "File extension not correct. Must be .fastq file. Skipping $sample " | tee -a logs/stderr
+            echo "File extension not correct. Must be .fastq file. Skipping $sample " >&2
             continue #Para que salte al siguiente archivo si no es un .fastq.gz
     fi 
 
     #Vamos a comprobar que el archivo es legible y ejecutable
-    echo "Checking $sample permissions" | tee -a logs/stdout
+    echo "Checking $sample permissions" 
 
     if [[ -r "$file" && -x "$file" ]]; then
-            echo "File is readable and executable" | tee -a logs/stdout
+            echo "File is readable and executable" 
     elif [ -r "$file" ]; then
-            echo "File is readable but not executable. Fixing." | tee -a logs/stdout
+            echo "File is readable but not executable. Fixing." >&2
             chmod +x "$file"
-            echo "fixed" | tee -a logs/stdout
+            echo "fixed" >&2
     elif [ -x "$file" ]; then
-            echo "File is executable but not readable" | tee -a logs/stdout
+            echo "File is executable but not readable" >&2
             chmod +r "$file"
-            echo "Fieexd" | tee -a logs/stdout
+            echo "Fieexd" >&2
     else
-            echo "File is neither readable nor executable. Fixing" | tee -a logs/stdout
+            echo "File is neither readable nor executable. Fixing" >&2
             chmod +rx "$file"
-            echo "Fixed" | tee -a logs/stdout
+            echo "Fixed" >&2
     fi
 
     
     #Crearemos un link simbólico para cada archivo que se guardará en este directorio
-    echo "Creating symbolic link for $sample ..." | tee -a logs/stdout
+    echo "Creating symbolic link for $sample ..." 
     ln -s $file $sample   #Coge de la ruta del directorio dado cada archivo que haya en él y lo guarda como un symlink en este directorio
     mv $sample results/
     #Vamos a comprobar que haya ido bien la creación del link
     if [ $? != 0 ]; then
-        echo "Something went wrong during symbolic links creation for file $sample" | tee -a logs/stderr
+        echo "Something went wrong during symbolic links creation for file $sample" >&2
         exit 1  
     fi
-    echo "Symbolic link created for $file and named $sample" | tee -a logs/stdout
+    echo "Symbolic link created for $file and named $sample" 
 done
-echo -e "\n"
-echo "Symbolic links successfully created in $(pwd)" | tee -a logs/stdout
+} >> >(tee -a logs/${name}.out) 2>> >(tee -a logs/${name}.err)
+
+echo -e "\n" >> >(tee -a logs/all_files.out)
+echo "Symbolic links successfully created in $(pwd)" >> >(tee -a logs/all_files.out)
 
