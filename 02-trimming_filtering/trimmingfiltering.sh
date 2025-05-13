79ec18f1a6934d5d0431551bf51ee959efa194ec
lau-bgg <laurabarrealesgomez@MacBook-Air-de-Laura.local>
2025-05-14 01:22:42 +0200

    no reconoce trimmomatic

--- 02-trimming_filtering/trimmingfiltering.sh
+++ 02-trimming_filtering/trimmingfiltering.sh
@@ -3,7 +3,7 @@
 #Los archivos que acepta solo son archivos .fastq 
 #Author: Laura Barreales and Sara Lévano
 #Start date: 6th May 2025
-version="Version: 2.1"
+version="Version: 3.0"
 
 #Usage example: trimmingfiltering.sh
 #No arguments expected, but -v and -h are available
@@ -15,26 +15,30 @@ version="Version: 2.1"
 #fastqc_dir="./results/fastqc"
 #multiqc_dir="./results/multiqc"
 
+
 #TENEMOS QUE CREAR UNA CARPETA PARA MULTIQC Y FASTQC DENTRO DE LOS LOGS
 
 #######################################################################
+# inicialización de logs vacíos
+cat /dev/null > logs/*-v
 
-#explain the code
-{ echo -e "Use -h for help or -v for the version. \nBut do not introduce any arguments or options for for trimming and filtering sequences" | tee -a logs/stdout
+#explain the codec
+{ echo -e "Use -h for help or -v for the version."
 
 #Parámetros por defecto de Trimmomatic por si el usuario no pone ninguno
 #Si el usuario pone alguno, estas variables se reescribiran gracias al getopts 
 SLIDINGWINDOW="4:20"
-MINLEN="36"
-LEADING="10"
-TRAILING="10"
+MINLEN="40" #ente 36-50 suele estar bien
+LEADING="3" #como elimina desde el principio donde la secuenciación tiene baja calidad, evita cortar demaisado
+TRAILING="20" #elimina desde atrás pb con phredscore menor a 20
+ILLUMINACLIP="TruSeq3-PE-2.fa:2:30:10" #contiene el archivo de los adaptadores: el máximo de mismatch que se aceptan:palindromeClipthreshold:simpleCLipThreshold
 
 #Manejar opciones y argumentos
-while getopts "hvm:w:l:t:i:o:f:c:" opt; do
+while getopts "hvm:w:l:t:i:o:f:c:p:" opt; do
     case $opt in
         h) echo -e "You asked for usage help\n"\
             "This script trims and filters raw sequences" 
-            echo -e "Usage example:\n\t./$0 -h: Provides help \n\t./$0 -v: Tells script's version\n\t./$0 -w -m -l -t -i -o -f -c: trims and filters raw sequences" 
+            echo -e "Usage example:\n\t$0 -h: Provides help \n\t$0 -v: Tells script's version\n\t$0 -w -m -l -t -i -o -f -c: trims and filters raw sequences" 
             exit 0;;
         v) echo "$version" 
             exit 0;;
@@ -46,17 +50,47 @@ while getopts "hvm:w:l:t:i:o:f:c:" opt; do
         o) output_dir="$OPTARG" ;;
         f) fastqc_dir="$OPTARG" ;;
         c) multiqc_dir="$OPTARG" ;;
-        \?) echo -e "Invalid option\n"\ 
-            "Usage example: $0 " >&2
+        p) ILLUMINACLIP="$OPTARG" ;;
+        \?) echo -e "Invalid option\nUsage example: $0 " >&2
             exit 1;; #si pone simbolos raros no sirve
     esac
 done
 echo "Usando SLIDINGWINDOW=$SLIDINGWINDOW y MINLEN=$MINLEN" 
 } 2>> >(tee -a logs/all_files.err) >> >(tee -a logs/all_files.out) 
+#mkdir -p "$output_dir" "$fastqc_dir" "$multiqc_dir" 
 
-for file in "$input_dir"/*; do #Los archivos fastq para trimar y filtrar se encuentran en esa carpeta. $file debería ser una ruta ansoluta al file
-    sample=$(basename "$file") #Nos quedamos con el nombre de la muestra. Falta quitarle el .fastq, ya veremos como hacerlo en clase
-    name=${sample%.*} #quitar la extension del archivo para normbrar los logs
+
+#Comprobar que los directorios existan
+echo -e "\nChecking if output directory exists" 
+if [ ! -d "$input_dir" ]
+then 
+     echo "$input_dir does not path exist"
+fi
+#Comprobar que los directorios existan
+echo -e "\nChecking if output directory exists" 
+if [ ! -d "$output_dir" ]
+then 
+     echo "$output_dir does not path exist"
+fi
+#Comprobar que los directorios existan
+echo -e "\nChecking if output directory exists" 
+if [ ! -d "$fastqc_dir" ]
+then 
+     echo "$fastqc_dir does not path exist"
+fi
+#Comprobar que los directorios existan
+echo -e "\nChecking if output directory exists" 
+if [ ! -d "$multiqc_dir" ]
+then 
+     echo "$multiqc_dir does not path exist"
+fi
+
+#crea los directorios que especifíca
+for file in "$input_dir"/*_1.fastq.gz; do #Los archivos fastq para trimar y filtrar se encuentran en esa carpeta. $file debería ser una ruta ansoluta al file
+    sample=$(basename "$file" "_1.fastq.gz") #Nos quedamos con el nombre de la muestra.  
+    #name=${sample%.*} #Para quitar la extensión y poder poner el nombre de la muestra en los archivos input y putput
+    file1="${input_dir}/${sample}_1.fastq.gz"
+    file2="${input_dir}/${sample}_2.fastq.gz" #coge la muestra pareada 
     #Vamos a comprobar que el archivo no está vacío
     { echo "Cheking if the "$sample" exists and it's not empty" 
     if [ -s "$file" ]; then
@@ -67,48 +101,40 @@ for file in "$input_dir"/*; do #Los archivos fastq para trimar y filtrar se encu
     fi 
 
     #Vamos a comprobar que el arhivo es .fastq
-    echo "Checking whether file extension is .fastq" 
-    if [[ "$file" == *.fastq ]]; then
+    echo "Checking whether file extension is .fastq.gz" 
+    if [[ "$file" == *.fastq.gz ]]; then
             echo "File extension is correct" 
     else
-            echo "File extension not correct. Must be .txt file" >&2
+            echo "File extension not correct. Must be .fastq.gz file" >&2
             exit 1
     fi 
 
-    #Vamos a comprobar que el archivo es legible y ejecutable
+    #Vamos a comprobar que el archivo es legible 
     echo "Checking "$sample" permissions" 
 
-    if [[ -r "$file" && -x "$file" ]]; then
-            echo "File is readable and executable" 
-    elif [ -r "$file" ]; then
-            echo "File is readable but not executable. Fixing." >&2
-            chmod +x "$file"
-            echo "fixed" >&2
-    elif [ -x "$file" ]; then
-            echo "File is executable but not readable" >&2
-            chmod +r "$file"
-            echo "Fieexd" >&2
+    if [[ -r "$file" ]]; then #solo necesito que sea legible
+        echo "File is readable"
     else
-            echo "File is neither readable nor executable. Fixing" >&2
-            chmod +rx "$file"
-            echo "Fixed" >&2
+        echo "File is not readable. Fixing." >&2
+        chmod +r "$file"
     fi
-    } 2>> >(tee -a logs/${name}.err)  >> >(tee -a logs/${name}.out) 
+
+    } 2>> >(tee -a logs/${sample}.err)  >> >(tee -a logs/${sample}.out) 
     #Ahora vamos a trimar. 
     #Necesitamos saber como se llaman los archivos fastq y cómo se diferencian entre pares de secuencias
     #Va a sacar 4 archivos, dos de ellos pasarán la selección tras el trimado (paired) y otros dos no (unpaired). Esto ocurre porque son secuencias pareadas
     echo "Processing "$sample"..."
-    name=${sample%.*} #Para quitar la extensión y poder poner el nombre de la muestra en los archivos input y putput
+    #name=${sample%.*} #Para quitar la extensión y poder poner el nombre de la muestra en los archivos input y putput
     {
-    trimmomatic PE \
-      ${name}_R1.fastq ${name}_R2.fastq \
-      ${name}_R1_paired.fastq ${name}_R1_unpaired.fastq \
-      ${name}_R2_paired.fastq ${name}_R2_unpaired.fastq \
-      ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+      java -jar trimmomatic-0.39.jar PE -phred33 \
+      "$file1" "$file2" \
+      "$output_dir/${sample}_1_paired.fastq.gz" "$output_dir/${sample}_1_unpaired.fastq.gz" \
+      "$output_dir/${sample}_2_paired.fastq.gz" "$output_dir$/{sample}_2_unpaired.fastq.gz" \
+      ILLUMINACLIP:$ILLUMINACLIP \
       SLIDINGWINDOW:$SLIDINGWINDOW \
       LEADING:$LEADING TRAILING:$TRAILING \
       MINLEN:$MINLEN
-     } 2>> >(tee -a logs/${name}_TRIM.err) >> >(tee -a logs/${name}_TRIM.out) 
+     } 2>> >(tee -a logs/${sample}_TRIM.err) >> >(tee -a logs/${sample}_TRIM.out) 
       #MINLEN: Es la cantidad mínima de bases que pedimos tras el trimado, yo pondría más
       #LEADING: Es la calidad mínima que debe tener la secuenciación de cada base del principio, si no llega se corta
       #TRAILING: Es la calidad mínima que debe tener la secuenciación de cada base del final, si no llega se corta
@@ -120,14 +146,16 @@ for file in "$input_dir"/*; do #Los archivos fastq para trimar y filtrar se encu
       #Hacaer Fastqc para cada muestra
       {
       echo "Executing FastQC for paired files of $sample" | tee -a logs/stdout
-      fastqc "$output_dir/${name}_R1_paired.fastq" "$output/${name}_R2_paired.fastq" -o "$fastqc_dir"
-      } 2>> >(tee -a logs/fastqc/${name}_fastqc.err)  >> >(tee -a logs/fastqc/${name}_fastqc.out)
+      fastqc "$output_dir/${sample}_1_paired.fastq.gz" "$output_dir/${sample}_2_paired.fastq.gz" -o "$fastqc_dir"
+      } 2>> >(tee -a logs/fastqc/${sample}_fastqc.err)  >> >(tee -a logs/fastqc/${sample}_fastqc.out)
 done
 
 
 # Hacer multiqc para integrar el análisis del fastqc
 { echo "Running MultiQC..." | tee -a logs/stdout
-multiqc "$multiqc_dir" -n "multiqc_trimmed_analysis.html" -o "$multiqc_dir" } 2>> >(tee -a logs/multiqc/multiqc.err) >> >(tee -a logs/multiqc/multiqc.out)
+multiqc "$fastqc_dir" -n "multiqc_trimmed_analysis.html" -o "$multiqc_dir" } 2>> >(tee -a logs/multiqc/multiqc.err) >> >(tee -a logs/multiqc/multiqc.out)
    
 
 echo -e "Analysis completed." >> >(tee -a logs/all_files.out)
+###############################################
+#chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/TrimmomaticManual_V0.32.pdf
