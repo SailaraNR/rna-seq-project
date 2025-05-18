1f2e5e5b253a7d949e4e01b50877eaf203746a80
sailara.nr@gmail.com <sailara.nr@gmail.com>
2025-05-18 13:32:37 +0200

    Prueba de conteo

--- 05-counts_alignments/counts_alignments.sh
+++ 05-counts_alignments/counts_alignments.sh
@@ -8,7 +8,7 @@
 version="versión 1.0"
 
 # Usage example:
-# ./counts_alignment.sh -A genome_annotation.gtf -o output.txt -f sorted_example_alignment.bam
+# ./counts_alignment.sh -A genome_annotation.gtf -o output.txt -d dir_sorted_example_alignment
 #-v and -h are available for help and version
 
 #######################################################################
@@ -22,35 +22,54 @@ while getops "hvA:o:f:"; do
         exit 0;;
         A) GTF="$OPTARG";;
         o) output="$OPTARG";;
-        f) alignment="$OPTARG";;
+        d) alignment="$OPTARG";;
         \?) echo -e "Invalid option: This script counts the number of reads of an aligment using a bam file and  GTF\nUsage example:\n"\
-        "./counts_alignment.sh -A genome_annotation.gtf -o output.txt -f sorted_example_alignment.bam" | tee -a logs/stderr
+        "./counts_alignment.sh -A genome_annotation.gtf -o output.txt -d dir_sorted_example_alignment" >&2
         exit 1;;
     esac
-done
+done 2>> >(tee -a logs/all_files.err)  >> >(tee -a logs/all_files.out) 
 
 # File validation: Check if the variables exist
+# El GTF no es nuestro, así que tampoco podemos cambiar los permisos, solo verificar si existe o no
 if [[ ! -e "$GTF" ]] || [[ ! -r "$GTF" ]]; then
-	echo "Error: $GTF does not exist or does not have read permissions" | tee -a logs/stderr
+	echo "Error: $GTF does not exist or does not have read permissions" >&2
 	exit 1
-fi
+fi 2>> >(tee -a logs/all_files.err)  >> >(tee -a logs/all_files.out) 
 if [[ ! -e "$alignment" ]] || [[ ! -r "$alignment" ]]; then
-	echo "Error: $alignment does not exist or does not have read permissions" | tee -a logs/stderr
+	echo "Error: $alignment does not exist or does not have read permissions" >&2
 	exit 1
-fi
+fi 2>> >(tee -a logs/all_files.err)  >> >(tee -a logs/all_files.out) 
 
 #Create output file
 echo "Creating output.txt file..."
 if ! [[ -e $output ]]; then
-	echo "Output file does not exists, creating..." | tee -a logs/stout
+	echo "Output file does not exists, creating..."
 	touch $output
-fi
+fi 2>> >(tee -a logs/all_files.err)  >> >(tee -a logs/all_files.out)
+{
+for file in "$alignment"/SRR*/*.bam; do # 04-reads_alignment/results en nuestro caso sería el $alignment
+        sample=$(basename "$file" "_sorted.bam")
+        echo -e "\nChecking if $sample exists and it's not empty"
+        if [[ -f "$file" && -s "$file" ]]; then
+            echo "$sample exists and is not empty"
+        else
+            echo "$sample is not a file or is empty"  >&2
+            exit 1
+        fi
+        echo "Checking $sample permissions"
+            if [[ ! -r "$file" ]]; then
+                echo "File is not readable. Fixing..." >&2
+                chmod +r "$file" && echo "Permissions fixed" || echo "Could not change permisions" >&2
+                    continue
+            fi
+    done
 
-#Counting reads
-featureCounts -p -t exon -O -T 6 -a "$GTF" -o "$output" "$alignment" | tee -a logs/stdout 2>> logs/stderr
+    #Counting reads
+    featureCounts -p -O -T 6 -a "$GTF" -o "$output" "$file" && echo "Counts for "$sample" done" || echo "Counts for "$sample" failed"
+} 2>> >(tee -a logs/${sample}.err)  >> >(tee -a logs/${sample}.out)
 
 # -p species that fragments (or templates) will be counted instead of reads. This is only applicable for paired-end reads.
-# -t type of fragments (exon)
+# -t type of fragments (exon, intron...)
 # -O assigns reads to all their overlapping meta-features.
 # -T specifies the number (6) of threads to be used.
 # -a is the genome annotation file ($GTF).
