4f9638d66dc8784074097e1aff8e36bac519ae11
lau-bgg <laurabarrealesgomez@MacBook-Air-de-Laura.local>
2025-06-18 14:06:59 +0200

    Finalizado

--- 00-raw_data/00-data.sh
+++ 00-raw_data/00-data.sh
@@ -1,31 +1,28 @@
-# !/bin/bash
-#Este script acepta una URL que corresponde a un archivo con una lista de las accesion de las muestras que se queiren descargar
-#Se puede obetener este archivo desde la web de SRA correspondiente al experimento
+#!/bin/bash
 #Author: Laura Barreales y Sara Lévano
 #Start date: 1st may 2025
-version= Version 2.3
-
-#Usage example
-#00-data.sh -f SRR_Acc_List.txt
+#Purpose: This scripts accepts a file.txt containing a list of accessions of the samples you want to download
+#Usage example: 00-data.sh -f SRR_Acc_List.txt
 
 #Arguments expected: -f file.txt
+#Raw sequences will be downloaded in the current directory
 
 ############################################################################################
-
-# inicialización de logs vacíos
+readonly version= Version 2.3
+# Initializing empty logs
 cat /dev/null > logs/*.out
 cat /dev/null > logs/*.err
 
 # explain the code
 { echo "Use -f for the file, -h for help and -v for the version" 
 
-#Vamos a ver si se han proporcionado argumetnos
+#Check if arguments are provided
 if [ $# -eq 0 ]; then
         echo -e "No options provided. \nUse $0 -h for help" >&2
         exit 1
 fi
 
-#Manejar opciones y argumentos
+#Managing options and arguments
 while getopts ":f:hv" opt; do
         case $opt in
                 h) echo -e "You asked for usage help\n"\
@@ -36,7 +33,7 @@ while getopts ":f:hv" opt; do
                    exit 0;;
                 f) echo "Abriendo el archivo -f $file ..."  
                    file="$OPTARG" ;;
-                :) echo "Option -$OPTARG requieres an argument" >&2 #si pone solo -f no sirve
+                :) echo "Option -$OPTARG requieres an argument" >&2 #if only the flag is written, the script exits
                    exit 1 ;;
                 \?) echo -e "Invalid option or missing argument\n"\
                    "Usage example: $0 SRR_Acc_List.txt" >&2
@@ -46,7 +43,7 @@ while getopts ":f:hv" opt; do
 done
 } 2>> >(tee -a logs/all_samples.err) >> >(tee -a logs/all_samples.out)
 
-#Vamos a comprobar que el archivo no está vacío
+#Checking emptiness of the file
 { echo "Checking if the file exists and it's not empty" 
 if [ -s $file ]; then
         echo "File exist and is not empty"  
@@ -55,7 +52,7 @@ else
         exit 1
 fi 
 
-#Vamos a comprobar que el arhivo es .txt
+#Checking if file has a .txt extension
 echo "Checking whether file extension is .txt" 
 if [ "$file" == *.txt ]; then
         echo "File extension is correct" 
@@ -64,7 +61,7 @@ else
         exit 1
 fi 
 
-#Vamos a comprobar que el archivo es legible y ejecutable
+#Cheking if the file readable and executable
 echo "Checking $file permissions" 
 
 if [[ -r "$file" && -x "$file" ]]; then
@@ -84,16 +81,18 @@ else
 fi 
 } 2>> >(tee -a logs/all_samples.err) >> >(tee -a logs/all_samples.out)
 
-#Se leerán las accesion del archivo una a una y se descargaran las raw data correspondientes
+#Accesions will be read one by one and the corresponding raw_seqs will be downloaded
+#Converts .sra files into .fastq and it splits into two files if samples are paired
+#.sra files will be removed as they won't be used anymore
 { echo "Processing accesions from $file..." 
 while read -r ACCESSION; do
   name=$(sample%.*)
-  [[ -z "$ACCESSION" ]] && continue # comprueba que la linea que lee está o no vacía
+  [[ -z "$ACCESSION" ]] && continue 
   echo "Downloading $ACCESSION" 
-  prefetch "$ACCESSION" #descarga .sra files
-  fasterq-dump --split-files "$ACCESSION" #convierte .sra files en .fastq y los separa en dos archivos si se trata de lecturas pareadas
+  prefetch "$ACCESSION" 
+  fasterq-dump --split-files "$ACCESSION" 
 done < "$file"
-rm -r *.sra #para borrar los sra files descargados con prefetch que no sirven para nada
+rm -r *.sra 
 
 echo "Finished. FASTQ files downloaded in $(pwd)" 
 } 2>> >(tee -a logs/all_${name}.err) >> >(tee -a logs/all_${name}.out)
